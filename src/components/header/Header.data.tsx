035df440b0a79671d802257d1e844d4a025c8cf4
Omar <omareabdo@gmail.com>
2023-12-29 20:25:19 +0100

    Updating header information

--- src/components/header/Header.data.tsx
+++ src/components/header/Header.data.tsx
@@ -1,1 +1,40 @@
-// Header data
+import {
+  BeakerIcon,
+  SparklesIcon,
+  PaintBrushIcon,
+  CpuChipIcon,
+  ServerStackIcon
+} from "@heroicons/react/24/outline";
+
+export const articleCategories = [
+  {
+    name: "Frontend",
+    description: "Web design, SPAs, CLIs, and Browser APIs",
+    href: "#",
+    icon: PaintBrushIcon,
+  },
+  {
+    name: "Backend",
+    description: "Rest APIs, Backend architecture, and Databases",
+    href: "#",
+    icon: ServerStackIcon,
+  },
+  {
+    name: "Computer science",
+    description: "Algorithms, Data structure, and all the fancy stuff!",
+    href: "#",
+    icon: CpuChipIcon,
+  },
+  {
+    name: "CI/CD",
+    description: "Continues integration with Docker, Kubernetes, and Git",
+    href: "#",
+    icon: SparklesIcon,
+  },
+  {
+    name: "Others",
+    description: "Testing, AI tools, and other trends!",
+    href: "#",
+    icon: BeakerIcon,
+  },
+];
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
-import { Dialog, Popover } from "@headlessui/react";
+import React, { Fragment, useState } from "react";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { articleCategories } from "./Header.data";
 import Toggle from "../common/toggle/toggle";
+
+function classNames(...classes: (string | boolean)[]): string {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Header: React.FC = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -40,15 +46,80 @@ const Header: React.FC = () => {
           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
             Portfolio
           </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Articles
-          </a>
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+              Articles
+              <ChevronDownIcon
+                className="h-5 w-5 flex-none text-gray-400"
+                aria-hidden="true"
+              />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {articleCategories.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 "
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 ">
+                        <item.icon
+                          className="h-6 w-6 text-gray-600 group-hover:text-red-700"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="flex-auto">
+                        <a
+                          href={item.href}
+                          className="block font-semibold text-gray-900"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-gray-600">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
             Contact
           </a>
         </Popover.Group>
+
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <Toggle onClick={handleToggle} toggled={isToggled} />
+          <a
+            href="#"
+            className="flex pl-3 items-center text-gray-500 hover:text-gray-600"
+          >
+            <img
+              src="https://flagicons.lipis.dev/flags/4x3/gb.svg"
+              alt=""
+              className="h-auto w-5 flex-shrink-0"
+            />
+          </a>
+          <a
+            href="#"
+            className="flex pl-3 items-center text-gray-500 hover:text-gray-600"
+          >
+            <img
+              src="https://flagicons.lipis.dev/flags/4x3/de.svg"
+              alt=""
+              className="h-auto w-5 flex-shrink-0"
+            />
+          </a>
         </div>
       </nav>
       <Dialog
@@ -76,19 +147,40 @@ const Header: React.FC = () => {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-  
                 <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 "
                 >
                   Portfolio
                 </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 "
-                >
-                  Articles
-                </a>
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 ">
+                        Articles
+                        <ChevronDownIcon
+                          className={classNames(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 flex-none"
+                          )}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...articleCategories].map((item) => (
+                          <Disclosure.Button
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 "
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
                 <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 "
@@ -98,6 +190,28 @@ const Header: React.FC = () => {
               </div>
               <div className="py-6">
                 <Toggle onClick={handleToggle} toggled={isToggled} />
+              </div>
+              <div className="py-6 flex justify-between">
+                <a
+                  href="#"
+                  className="flex  items-center text-gray-500 hover:text-gray-600"
+                >
+                  <img
+                    src="https://flagicons.lipis.dev/flags/4x3/gb.svg"
+                    alt=""
+                    className="h-auto w-5 flex-shrink-0"
+                  />
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center text-gray-500 hover:text-gray-600"
+                >
+                  <img
+                    src="https://flagicons.lipis.dev/flags/4x3/de.svg"
+                    alt=""
+                    className="h-auto w-5 flex-shrink-0"
+                  />
+                </a>
               </div>
             </div>
           </div>
