958bcd5f42b051319e928dba84f3c6f0fbd17262
Omar <omareabdo@gmail.com>
2024-12-04 17:37:59 +0200

    Updating footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -16,13 +16,14 @@ const Footer: React.FC = () => {
             <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
                 <h3 className="text-sm font-semibold leading-6 text-gray-900">
-                  Solutions
+                  Open Source Projects
                 </h3>
                 <ul role="list" className="mt-6 space-y-4">
-                  {navigation.solutions.map((item) => (
+                  {navigation.projects.map((item) => (
                     <li key={item.name}>
                       <a
                         href={item.href}
+                        target="_blank"
                         className="text-sm leading-6 text-gray-600 hover:text-gray-900"
                       >
                         {item.name}
@@ -33,13 +34,14 @@ const Footer: React.FC = () => {
               </div>
               <div className="mt-10 md:mt-0">
                 <h3 className="text-sm font-semibold leading-6 text-gray-900">
-                  Support
+                  Data Science Articles
                 </h3>
                 <ul role="list" className="mt-6 space-y-4">
-                  {navigation.support.map((item) => (
+                  {navigation.ds_articles.map((item) => (
                     <li key={item.name}>
                       <a
                         href={item.href}
+                        target="_blank"
                         className="text-sm leading-6 text-gray-600 hover:text-gray-900"
                       >
                         {item.name}
@@ -52,13 +54,14 @@ const Footer: React.FC = () => {
             <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
                 <h3 className="text-sm font-semibold leading-6 text-gray-900">
-                  Company
+                  Web Articles
                 </h3>
                 <ul role="list" className="mt-6 space-y-4">
-                  {navigation.company.map((item) => (
+                  {navigation.web_articles.map((item) => (
                     <li key={item.name}>
                       <a
                         href={item.href}
+                        target="_blank"
                         className="text-sm leading-6 text-gray-600 hover:text-gray-900"
                       >
                         {item.name}
@@ -69,13 +72,14 @@ const Footer: React.FC = () => {
               </div>
               <div className="mt-10 md:mt-0">
                 <h3 className="text-sm font-semibold leading-6 text-gray-900">
-                  Legal
+                  Pages
                 </h3>
                 <ul role="list" className="mt-6 space-y-4">
-                  {navigation.legal.map((item) => (
+                  {navigation.more_pages.map((item) => (
                     <li key={item.name}>
                       <a
                         href={item.href}
+                        target="_blank"
                         className="text-sm leading-6 text-gray-600 hover:text-gray-900"
                       >
                         {item.name}
@@ -87,45 +91,14 @@ const Footer: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24 lg:flex lg:items-center lg:justify-between">
-          <div>
-            <h3 className="text-sm font-semibold leading-6 text-gray-900">
-              Subscribe to our newsletter
-            </h3>
-            <p className="mt-2 text-sm leading-6 text-gray-600">
-              The latest news, articles, and resources, sent to your inbox
-              weekly.
-            </p>
-          </div>
-          <form className="mt-6 sm:flex sm:max-w-md lg:mt-0">
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              type="email"
-              name="email-address"
-              id="email-address"
-              autoComplete="email"
-              required
-              className="w-full min-w-0 appearance-none rounded-md border-0 bg-white px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-700 sm:w-56 sm:text-sm sm:leading-6"
-              placeholder="Enter your email"
-            />
-            <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-              <button
-                type="submit"
-                className="flex w-full items-center justify-center rounded-md bg-red-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-700"
-              >
-                Subscribe
-              </button>
-            </div>
-          </form>
-        </div>
+
         <div className="mt-8 border-t border-gray-900/10 pt-8 md:flex md:items-center md:justify-between">
           <div className="flex space-x-6 md:order-2">
             {navigation.social.map((item) => (
               <a
                 key={item.name}
                 href={item.href}
+                target="_blank"
                 className="text-gray-400 hover:text-gray-500"
               >
                 <span className="sr-only">{item.name}</span>
@@ -134,7 +107,7 @@ const Footer: React.FC = () => {
             ))}
           </div>
           <p className="mt-8 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
-            &copy; {new Date().getFullYear()} Your Company, Inc. All rights
+            &copy; {new Date().getFullYear()} Omar Abdo. All rights reserved
             reserved.
           </p>
         </div>
